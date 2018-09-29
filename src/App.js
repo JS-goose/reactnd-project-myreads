c86313958eee8ea1947a2e4ee7c08cb971900770
Jonathan Sexton <joncsexton@gmail.com>
2018-09-29 16:26:22 -0500

    Added export for components and import for App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import { Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import SearchPage from "./components/SearchPage";
+import Shelf from "./components/Shelf";
+import Book from "./components/Book";
 
 class BooksApp extends React.Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={ HomePage } />
+        <Route exact path="/" component={HomePage} />
       </div>
     );
 
