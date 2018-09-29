71b8639e361c8af48bc0abeadf0a98777706aeb1
Jonathan Sexton <joncsexton@gmail.com>
2018-09-29 18:18:21 -0500

    Added ol tag to be parent in book file

--- src/components/shelf.js
+++ src/components/shelf.js
@@ -8,9 +8,7 @@ class Shelf extends Component {
       <div className="bookshelf">
         <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
-          <ol className="books-grid">
-            <Book />
-          </ol>
+          <Book />
         </div>
       </div>
     );
