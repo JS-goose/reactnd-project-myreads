527e191b2386961d16b3563a2fefe8a573b26646
Jonathan Sexton <joncsexton@gmail.com>
2018-09-29 18:14:18 -0500

    Abstracted book and shelf out to components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import SearchPage from "./components/SearchPage";
-import Shelf from "./components/Shelf";
-import Book from "./components/Book";
+// import Shelf from "./components/Shelf";
+// import Book from "./components/Book";
 
 class BooksApp extends React.Component {
   render() {
     return (
       <div>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/" component={Shelf} />
         <Route exact path="/search" component={SearchPage} />
       </div>
     );
