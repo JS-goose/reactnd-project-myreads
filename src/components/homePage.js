033a549940b670db3d606d037997b1615d64e983
Jonathan Sexton <joncsexton@gmail.com>
2018-09-29 18:20:18 -0500

    Moved API import to home page component

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
 import Shelf from "./Shelf";
 
 class HomePage extends Component {
