01b371ea9cf8207c48185a7d7bb21c401309750b
Jonathan Sexton <joncsexton@gmail.com>
2018-09-29 16:23:19 -0500

    Moving main page structure to component

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,1 +1,11 @@
 import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>Create Contact!</div>
+        );
+    }
+}
+
+export default HomePage
