7229205ae632ada1450c11cff03a38581810a8bb
Jonathan Sexton <joncsexton@gmail.com>
2018-09-30 15:03:10 -0500

    Search page: setting up constructor and updateShelf method

--- src/components/searchPage.js
+++ src/components/searchPage.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
 
 class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    };
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((response) => {
+      this.setState({ books: response });
+    });
+  }
+
+  updateShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then((response) => {
+      book.shelf = shelf;
+      this.setState((state) => ({
+        books: state.books.filter((b) => b.id !== book.id).concat([book]),
+      }));
+    });
+  };
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link 
-          to="/"
-          className="close-search">Close</Link>
+          <Link to="/" className="close-search">
+            Close
+          </Link>
           <div className="search-books-input-wrapper">
             {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
